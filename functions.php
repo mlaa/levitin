773b44844e706f93db4836be11923b6da7c2cbf5
Ryan Williams <ryrowi@gmail.com>
2016-02-16 15:09:55 -0500

    move levitin_edit_profile_field into context

--- functions.php
+++ functions.php
@@ -46,34 +46,3 @@ foreach ( $cpwpst_includes as $file ) {
 	require_once $filepath;
 }
 unset($file, $filepath);
-
-/**
- * for edit view. use like bp_the_profile_field().
- * works inside or outside the fields loop.
- * TODO optimize: find some way to look up fields directly rather than (re)winding the loop every time.
- *
- * @param $field_name
- * @global $profile_template
- */
-function levitin_edit_profile_field( $field_name ) {
-	global $profile_template;
-
-	$profile_template->rewind_fields(); // reset the loop
-
-	while ( bp_profile_fields() ) {
-		bp_the_profile_field();
-
-		if ( bp_get_the_profile_field_name() !== $field_name ) {
-			continue;
-		}
-
-		$field_type = bp_xprofile_create_field_type( bp_get_the_profile_field_type() );
-		$field_type->edit_field_html();
-
-		do_action( 'bp_custom_profile_edit_fields_pre_visibility' );
-		bp_profile_visibility_radio_buttons();
-
-		do_action( 'bp_custom_profile_edit_fields' );
-		break; // once we output the field we want, no need to continue looping
-	}
-}
--- lib/mla/portfolios.php
+++ lib/mla/portfolios.php
@@ -85,3 +85,34 @@ function mla_xprofile_filter_link_profile_data( $field_value, $field_type = 'tex
 	return $values;
 }
 add_filter( 'bp_get_the_profile_field_value', 'mla_xprofile_filter_link_profile_data', 9, 2 );
+
+/**
+ * for edit view. use like bp_the_profile_field().
+ * works inside or outside the fields loop.
+ * TODO optimize: find some way to look up fields directly rather than (re)winding the loop every time.
+ *
+ * @param $field_name
+ * @global $profile_template
+ */
+function levitin_edit_profile_field( $field_name ) {
+	global $profile_template;
+
+	$profile_template->rewind_fields(); // reset the loop
+
+	while ( bp_profile_fields() ) {
+		bp_the_profile_field();
+
+		if ( bp_get_the_profile_field_name() !== $field_name ) {
+			continue;
+		}
+
+		$field_type = bp_xprofile_create_field_type( bp_get_the_profile_field_type() );
+		$field_type->edit_field_html();
+
+		do_action( 'bp_custom_profile_edit_fields_pre_visibility' );
+		bp_profile_visibility_radio_buttons();
+
+		do_action( 'bp_custom_profile_edit_fields' );
+		break; // once we output the field we want, no need to continue looping
+	}
+}
