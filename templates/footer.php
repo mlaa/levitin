6146e0efef06453bfd6ce5ce12dd7c25ba2206b4
Ryan Williams <ryrowi@gmail.com>
2016-03-04 16:47:23 -0500

    import footer from mla.org

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,33 @@
-<footer class="content-info" role="contentinfo">
-	<?php dynamic_sidebar( 'sidebar-footer' ); ?>
+<footer id="FOOTER_1">
+	<div id="DIV_2">
+		<div id="DIV_3">
+			<ul id="UL_4">
+				<li id="LI_5">
+					 <a href="/About-Us/About-the-MLA" id="A_6"><span id="SPAN_7"></span>About the MLA</a>
+				</li>
+				<li id="LI_8">
+					 <a href="/About-Us/Contact" id="A_9"><span id="SPAN_10"></span>Contact</a>
+				</li>
+				<li id="LI_11">
+					 <a href="/About-Us/Support/Donate-to-the-MLA" id="A_12"><span id="SPAN_13"></span>Donate to the MLA</a>
+				</li>
+			</ul>
+		</div>
+		<div id="DIV_14">
+			<ul id="UL_15">
+				<li id="LI_16">
+					<a href="https://www.facebook.com/modernlanguageassociation/" id="A_17"></a>
+				</li>
+				<li id="LI_18">
+					<a href="https://www.linkedin.com/company/modern-language-association" id="A_19"></a>
+				</li>
+				<li id="LI_20">
+					<a href="https://twitter.com/mlanews" id="A_21"></a>
+				</li>
+			</ul>
+		</div>
+		<p id="P_22">
+			© 2016 Modern Language Association of America
+		</p>
+	</div>
 </footer>
