eb0ef69192a3004fecdb7b2562e73192edbfe93b
Ryan Williams <ryrowi@gmail.com>
2016-03-18 16:10:37 -0400

    apply global max-width of 1000px to match old layout

--- templates/header.php
+++ templates/header.php
@@ -1,16 +1,18 @@
 <header id="main-site-header" class="banner" role="banner">
-	<a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<img src="<?php bloginfo('template_directory'); ?>/assets/images/mla-commons-logo.png" alt="<?php bloginfo( 'name' ); ?>">
-	</a>
-	<nav role="navigation" class="primary">
-		<?php
-		if ( has_nav_menu( 'primary_navigation' ) ) :
-			wp_nav_menu([
-				'theme_location' => 'primary_navigation',
-				'menu_class' => 'nav'
-			]);
-		endif;
-		?>
-	</nav>
-	<?php // get_search_form( true ); ?>
+	<div>
+		<a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+			<img src="<?php bloginfo('template_directory'); ?>/assets/images/mla-commons-logo.png" alt="<?php bloginfo( 'name' ); ?>">
+		</a>
+		<nav role="navigation" class="primary">
+			<?php
+			if ( has_nav_menu( 'primary_navigation' ) ) :
+				wp_nav_menu([
+					'theme_location' => 'primary_navigation',
+					'menu_class' => 'nav'
+				]);
+			endif;
+			?>
+		</nav>
+		<?php // get_search_form( true ); ?>
+	</div>
 </header>
