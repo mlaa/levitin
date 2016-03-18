b975263497474537709ea32974b06132e33c1ab7
Ryan Williams <ryrowi@gmail.com>
2016-03-18 17:01:24 -0400

    move members subnav outside main container to allow spanning full window width

--- buddypress/members/index.php
+++ buddypress/members/index.php
@@ -13,6 +13,26 @@
  */
 do_action( 'bp_before_directory_members_page' ); ?>
 
+<div class="item-list-tabs" role="navigation">
+	<ul>
+		<li class="selected" id="members-all"><a href="<?php bp_members_directory_permalink(); ?>"><?php printf( __( 'All Members %s', 'buddypress' ), '<span>' . bp_get_total_member_count() . '</span>' ); ?></a></li>
+
+		<?php if ( is_user_logged_in() && bp_is_active( 'friends' ) && bp_get_total_friend_count( bp_loggedin_user_id() ) ) : ?>
+			<li id="members-personal"><a href="<?php echo bp_loggedin_user_domain() . bp_get_friends_slug() . '/my-friends/'; ?>"><?php printf( __( 'My Friends %s', 'buddypress' ), '<span>' . bp_get_total_friend_count( bp_loggedin_user_id() ) . '</span>' ); ?></a></li>
+		<?php endif; ?>
+
+		<?php
+
+		/**
+		 * Fires inside the members directory member types.
+		 *
+		 * @since 1.2.0
+		 */
+		do_action( 'bp_members_directory_member_types' ); ?>
+
+	</ul>
+</div><!-- .item-list-tabs -->
+
 <div id="buddypress">
 
 	<?php
@@ -47,26 +67,6 @@ do_action( 'bp_before_directory_members_page' ); ?>
 	do_action( 'bp_before_directory_members_tabs' ); ?>
 
 	<form action="" method="post" id="members-directory-form" class="dir-form">
-
-		<div class="item-list-tabs" role="navigation">
-			<ul>
-				<li class="selected" id="members-all"><a href="<?php bp_members_directory_permalink(); ?>"><?php printf( __( 'All Members %s', 'buddypress' ), '<span>' . bp_get_total_member_count() . '</span>' ); ?></a></li>
-
-				<?php if ( is_user_logged_in() && bp_is_active( 'friends' ) && bp_get_total_friend_count( bp_loggedin_user_id() ) ) : ?>
-					<li id="members-personal"><a href="<?php echo bp_loggedin_user_domain() . bp_get_friends_slug() . '/my-friends/'; ?>"><?php printf( __( 'My Friends %s', 'buddypress' ), '<span>' . bp_get_total_friend_count( bp_loggedin_user_id() ) . '</span>' ); ?></a></li>
-				<?php endif; ?>
-
-				<?php
-
-				/**
-				 * Fires inside the members directory member types.
-				 *
-				 * @since 1.2.0
-				 */
-				do_action( 'bp_members_directory_member_types' ); ?>
-
-			</ul>
-		</div><!-- .item-list-tabs -->
 
 		<div id="members-dir-list" class="members dir-list">
 			<?php bp_get_template_part( 'members/members-loop' ); ?>
