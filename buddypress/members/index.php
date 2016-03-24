6fef792be4640b1fb5a998ee50428fd4dc0cbfbe
Ryan Williams <ryrowi@gmail.com>
2016-03-24 16:56:47 -0400

    restore default subnav on members directory

--- buddypress/members/index.php
+++ buddypress/members/index.php
@@ -13,26 +13,6 @@
  */
 do_action( 'bp_before_directory_members_page' ); ?>
 
-<div class="item-list-tabs" role="navigation">
-	<ul>
-		<li class="selected" id="members-all"><a href="<?php bp_members_directory_permalink(); ?>"><?php printf( __( 'All Members %s', 'buddypress' ), '<span>' . bp_get_total_member_count() . '</span>' ); ?></a></li>
-
-		<?php if ( is_user_logged_in() && bp_is_active( 'friends' ) && bp_get_total_friend_count( bp_loggedin_user_id() ) ) : ?>
-			<li id="members-personal"><a href="<?php echo bp_loggedin_user_domain() . bp_get_friends_slug() . '/my-friends/'; ?>"><?php printf( __( 'My Friends %s', 'buddypress' ), '<span>' . bp_get_total_friend_count( bp_loggedin_user_id() ) . '</span>' ); ?></a></li>
-		<?php endif; ?>
-
-		<?php
-
-		/**
-		 * Fires inside the members directory member types.
-		 *
-		 * @since 1.2.0
-		 */
-		do_action( 'bp_members_directory_member_types' ); ?>
-
-	</ul>
-</div><!-- .item-list-tabs -->
-
 <div id="buddypress">
 
 	<?php
@@ -67,6 +47,60 @@ do_action( 'bp_before_directory_members_page' ); ?>
 	do_action( 'bp_before_directory_members_tabs' ); ?>
 
 	<form action="" method="post" id="members-directory-form" class="dir-form">
+
+		<div class="item-list-tabs" role="navigation">
+			<ul>
+				<li class="selected" id="members-all"><a href="<?php bp_members_directory_permalink(); ?>"><?php printf( __( 'All Members %s', 'buddypress' ), '<span>' . bp_get_total_member_count() . '</span>' ); ?></a></li>
+
+				<?php if ( is_user_logged_in() && bp_is_active( 'friends' ) && bp_get_total_friend_count( bp_loggedin_user_id() ) ) : ?>
+					<li id="members-personal"><a href="<?php echo bp_loggedin_user_domain() . bp_get_friends_slug() . '/my-friends/'; ?>"><?php printf( __( 'My Friends %s', 'buddypress' ), '<span>' . bp_get_total_friend_count( bp_loggedin_user_id() ) . '</span>' ); ?></a></li>
+				<?php endif; ?>
+
+				<?php
+
+				/**
+				 * Fires inside the members directory member types.
+				 *
+				 * @since 1.2.0
+				 */
+				do_action( 'bp_members_directory_member_types' ); ?>
+
+			</ul>
+		</div><!-- .item-list-tabs -->
+
+		<div class="item-list-tabs" id="subnav" role="navigation">
+			<ul>
+				<?php
+
+				/**
+				 * Fires inside the members directory member sub-types.
+				 *
+				 * @since 1.5.0
+				 */
+				do_action( 'bp_members_directory_member_sub_types' ); ?>
+
+				<li id="members-order-select" class="last filter">
+					<label for="members-order-by"><?php _e( 'Order By:', 'buddypress' ); ?></label>
+					<select id="members-order-by">
+						<option value="active"><?php _e( 'Last Active', 'buddypress' ); ?></option>
+						<option value="newest"><?php _e( 'Newest Registered', 'buddypress' ); ?></option>
+
+						<?php if ( bp_is_active( 'xprofile' ) ) : ?>
+							<option value="alphabetical"><?php _e( 'Alphabetical', 'buddypress' ); ?></option>
+						<?php endif; ?>
+
+						<?php
+
+						/**
+						 * Fires inside the members directory member order options.
+						 *
+						 * @since 1.2.0
+						 */
+						do_action( 'bp_members_directory_order_options' ); ?>
+					</select>
+				</li>
+			</ul>
+		</div>
 
 		<div id="members-dir-list" class="members dir-list">
 			<?php bp_get_template_part( 'members/members-loop' ); ?>
